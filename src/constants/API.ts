bc22fd21cc73d889d0a5a576ec22d006ec2cc824
shenjiaolong <shenjiaolong@welue.com>
2021-05-19 11:25:27 +0800

    [fix] 环境切换

--- src/constants/API.ts
+++ src/constants/API.ts
@@ -1,11 +1,14 @@
 let API_HOSTNAME;
+let UPLOAD_NAME;
 
 if (process.env.NODE_ENV === 'development') {
   API_HOSTNAME = '/media/MIS';
+  UPLOAD_NAME = '/files';
 } else {
   API_HOSTNAME = '/media/MIS';
+  UPLOAD_NAME = '/file';
 }
 
-export const UPLOADNAME = '/files'
+export const UPLOADNAME = UPLOAD_NAME;
 export const APIHOSTNAME = API_HOSTNAME;
 
